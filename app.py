dcf2ea3618c0f45e6c7260e66c3a695b35a65693
Knoy2012 <gdylan@y7mail.com>
2024-08-17 18:32:12 +1000

    Post Bluies Library Work

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 app.config["DEBUG"] = True
+app.secret_key = os.urandom(24)
 Session(app)
 
 db = SQL("sqlite:///beet.db")
@@ -29,14 +30,44 @@ def index():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    """Log user in"""
 
-    session.clear() # forget user_id 
+    if 'user_id' in session: # if user logged in, redirect the to homepage
+        return redirect(url_for('index'))
 
     if request.method == 'POST': # user has tried to login
+
+        if not request.form.get('username'): # if user hasn't entered username
+            flash('Username if required', 'error')
+            return render_template('login.html')
+        
+        elif not request.form.get('password'): # if user hasn't entered password
+            flash('Password is required', 'error')
+            return render_template('login.html')
+        
+        # get user data from users table
+        userDb = db.execute('SELECT * FROM users WHERE username=?', request.form.get('username'))
+
+        # check if password matches hash
+        if len(userDb) != 1 or not check_password_hash(userDb[0]['hash'], request.form.get('password')):
+            flash('Username or Password Incorrect', 'error')
+            return render_template('login.html')
+        
+        session['user_id'] = userDb[0]['id']
+        
         return redirect("/")
 
     else:   # GET request
         return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log the user out when logout pressed"""
+
+    session.clear()
+    flash('You have successfully been logged out', 'info')
+    return redirect(url_for('login'))
     
     
 @app.route("/register", methods=['GET', 'POST'])
@@ -48,27 +79,27 @@ def register():
 
         if not userReg: # force user to enter value
             flash('Username is required', 'error')
-            return redirect(url_for('register'))
+            return render_template('register.html')
         
         passReg = request.form.get('password')
         
         if not passReg or passReg != request.form.get('confirmation'):
             flash('Password Required / Passwords do not match', 'error')
-            return redirect(url_for('register'))
+            return render_template('register.html')
         
         userDb = db.execute('SELECT username FROM users WHERE username=?', userReg)
 
         if userDb:
             if userDb[0]['username'] == userReg:
                 flash('Username Already Taken', 'error')
-                return redirect(url_for('register'))
+                return render_template('register.html')
             
         db.execute('INSERT INTO users (username, hash) VALUES(?,?)', userReg, generate_password_hash(passReg, method='pbkdf2', salt_length=16))
 
-        userid = db.execute('SELECT id FROM users WHERE username=?', userReg)
-        session['id'] = userid[0]['id']
+        flash('Successfully Registered!', 'success')
+        print(session)
 
-        return redirect(url_for('survey')) # redirect user to SURVEY??? ############# FIX THIS #####################
+        return redirect(url_for('login')) # redirect user to SURVEY??? ############# FIX THIS #####################
     
     else:
         return render_template("register.html")
